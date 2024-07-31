151382da333717938ad1c1a3675258dbd70a1e33
CrLeCook <112937952+CrLeCook@users.noreply.github.com>
2024-07-31 09:57:16 +0100

    code reformat

--- project/core/views.py
+++ project/core/views.py
@@ -5,23 +5,18 @@ from django.db.models import Q
 from django.shortcuts import render, redirect
 
 from product.models import Product, Category
-
 from .forms import SignUpForm
 
 def frontpage(request):
-    products = Product.objects.all()[0:8]
-
+    products = Product.objects.all()[:8]
     return render(request, 'core/frontpage.html', {'products': products})
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-
         if form.is_valid():
             user = form.save()
-
             login(request, user)
-
             return redirect('/')
     else:
         form = SignUpForm()
@@ -41,8 +36,8 @@ def edit_myaccount(request):
         user.email = request.POST.get('email')
         user.username = request.POST.get('username')
         user.save()
-
         return redirect('myaccount')
+
     return render(request, 'core/edit_myaccount.html')
 
 def shop(request):
@@ -50,19 +45,17 @@ def shop(request):
     products = Product.objects.all()
 
     active_category = request.GET.get('category', '')
-
     if active_category:
         products = products.filter(category__slug=active_category)
 
     query = request.GET.get('query', '')
-
     if query:
         products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
     context = {
         'categories': categories,
         'products': products,
-        'active_category': active_category
+        'active_category': active_category,
     }
 
     return render(request, 'core/shop.html', context)
