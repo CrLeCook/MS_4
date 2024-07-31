0408931bdd75cc20a5a7c92306c5a7ba96ba4050
CrLeCook <112937952+CrLeCook@users.noreply.github.com>
2024-07-31 09:57:44 +0100

    code reformat

--- project/cart/views.py
+++ project/cart/views.py
@@ -1,17 +1,14 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
 from .cart import Cart
-
 from product.models import Product
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 
 def add_to_cart(request, product_id):
     cart = Cart(request)
     cart.add(product_id)
-
     return render(request, 'cart/partials/menu_cart.html')
 
 def cart(request):
@@ -19,8 +16,6 @@ def cart(request):
 
 def success(request):
     return render(request, 'cart/success.html')
-
-
 
 def update_cart(request, product_id, action):
     cart = Cart(request)
@@ -34,7 +29,7 @@ def update_cart(request, product_id, action):
         return JsonResponse({'status': 'error', 'message': 'Invalid action'}, status=400)
 
     quantity = cart.get_item(product_id)
-    
+
     if quantity:
         quantity = quantity['quantity']
         total_price = (quantity * product.price) / 100
@@ -53,14 +48,11 @@ def update_cart(request, product_id, action):
     else:
         item = None
 
-    return JsonResponse({
-        'item': item
-    })
-
+    return JsonResponse({'item': item})
 
 @login_required
 def checkout(request):
-    pub_key = settings.STRIPE_API_KEY_PUBLISHABLE 
+    pub_key = settings.STRIPE_API_KEY_PUBLISHABLE
     return render(request, 'cart/checkout.html', {'pub_key': pub_key})
 
 def hx_menu_cart(request):
